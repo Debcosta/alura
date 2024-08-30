54e9de3091c803b7c79aa999913780578f53be39
Debcosta <0000110578101xsp@al.educacao.sp.gov.br>
2024-08-30 10:39:18 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,11 @@
-let cor;
-let posicaoHorizontal; // x
-let posicaoVertical; // yt c
+const avanca = document.querySelectorAll('.btn-proximo');
 
-function setup() {
-  createCanvas(400, 400);
-   background(color(100,0,0));
-  cor = color(random(0,255), random(0,255), random(0,255));
-  posicaoHorizontal = 200;
-  posicaoVertical = 200;
-}
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-
-function draw() {
-  
-  fill(cor);
-  circle(posicaoHorizontal,posicaoVertical,50);
- 
-  
-  
-  if (mouseX < posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal - 1;
-  }
-  
-  if (mouseX > posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal + 1;
-  }
-  
-  if (mouseY < posicaoVertical){
-    posicaoVertical--;
-  }
-    
-  if (mouseY > posicaoVertical){
-    posicaoVertical++;
-  }
-  
-  if (mouseIsPressed){
-    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
-  }
-    
-}
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
